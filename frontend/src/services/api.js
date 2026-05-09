7904d3bc87b3aaddc8e1bcedad768795f94f3589
Megha Singh <meghashakya235@gmail.com>
2026-05-09 23:09:25 +0530

    update api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://noble-possibility-production-55ee.up.railway.app/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
